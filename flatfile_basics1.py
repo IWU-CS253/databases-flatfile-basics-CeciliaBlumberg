eb3377daec763a12d462873fa9879206107db064
bsheese <bsheese@iwu.edu>
2024-02-08 09:03:02 -0600

    Update and rename sql_basics1.py to flatfile_basics1.py

---
+++ flatfile_basics1.py
@@ -0,0 +1,32 @@
+# note! the requests package should be installed in pycharm before you begin
+import os 
+import requests
+
+# ignore this, don't edit
+def download_file(url, filename):
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+        print(f"File '{filename}' downloaded successfully.")
+    else:
+        print(f"Failed to download the file. Status code: {response.status_code}")
+
+if not os.path.isfile('favorites.csv'):
+    url = 'https://cdn.cs50.net/2023/fall/lectures/7/src7/favorites/favorites.csv'
+    filename = 'favorites.csv'
+    download_file(url, filename)
+
+
+
+# START HERE - this code mirrors CS50x lecture 7 code starting at 11:30
+with open("survey.csv", "r", encoding="utf-8") as file:
+    reader = csv.reader(file)
+    next(reader)
+    for row in reader:
+        print(row[1])
+
+
+
+
+
